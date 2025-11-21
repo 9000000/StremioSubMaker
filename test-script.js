8ebe4a493be3e508b7d868130812d03848fb03f5
xtremexq <xtremexq@users.noreply.github.com>
2025-11-21 07:29:05 -0300

    chore: remove unused backup and test script

--- test-script.js
+++
@@ -1,7 +0,0 @@
-const { parseConfig, encodeConfig } = require('./src/utils/config');
-(async()=>{
-  const sample={geminiApiKey:'x',sourceLanguages:['eng'],targetLanguages:['spa'],learnMode:true,learnPlacement:'top',learnOrder:'source-top',learnTargetLanguages:['spa']};
-  const b64=encodeConfig(sample);
-  const parsed=await parseConfig(b64,{isLocalhost:true});
-  console.log('parsed learnPlacement', parsed.learnPlacement);
-})();
